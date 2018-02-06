09873116e293aa2b3688c93fd38bece70a1ae985
floris09 <florismeininger@gmail.com>
2018-02-06 14:41:48 +0100

    cors

--- index.js
+++ index.js
@@ -11,16 +11,9 @@ let app = express()
 const server = http.Server(app)
 
 app
+  .use(cors())
   .use(bodyParser.urlencoded({ extended: true }))
   .use(bodyParser.json())
-  .use(cors())
-  .use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', 'https://florismeininger.herokuapp.com');
-    res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    next();
-      });
   .use(mailer)
 
 app.get('/', (req, res) => {
