ebd3cf57b4dadbf02da1b08cb09d8654fe2b08cf
floris09 <florismeininger@gmail.com>
2018-02-07 13:53:15 +0100

    back to normal

--- index.js
+++ index.js
@@ -11,22 +11,7 @@ let app = express()
 const server = http.Server(app)
 
 app
-  .use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
-    next();
-  });
+  .use(cors())
   .use(bodyParser.urlencoded({ extended: true }))
   .use(bodyParser.json())
   .use(mailer)
